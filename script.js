7f4ea0bb5b655dfc2067ceafde6dbf5327ab116f
Qas1m-akbar <qasimktk901@gmail.com>
2024-07-09 08:33:57 -0700

    all done

---
+++ script.js
@@ -0,0 +1,72 @@
+//METHOD 01 : trim() --> Removing extra spaces from the given string  //
+
+let string = "    Qasim akbar    ";
+string = string.trim();
+console.log(string);
+
+
+
+//METHOD 02 : substring() --> used for remove letter by passing index in function//
+
+// let string = "Qasim akbar";
+// string = string.substring(0,5);
+// console.log(string);
+
+
+
+//METHOD 03 : split() --> this function is for remove the given letter and convert string into an array//
+// let string = "Qasim-akbar";
+// string = string.split("-");
+// console.log(string);
+
+
+//METHOD 04 : join() --> To join all string collectively -->removing space//
+// let array = ["Qasim", "akbar" , "uzair"];
+// array = array.join("k");
+// console.log(array);
+
+
+
+
+
+//METHOD 05 : indexof() --> used for to check first letter index //
+// let string = "Qasim akbar";
+// string = string.indexOf("i");
+// console.log(string);
+
+
+
+//METHOD 06 : substring() --> remove letter from index 5 to 10//
+// let string = "Qasim akbar";
+// let name = string.substring(5,10);
+// console.log(name);
+
+
+
+//METHOD 07 : lastindexof() --> used for check the last index of the string//
+
+
+// let string ="Qasim akbar akbar";
+// string = string.lastIndexOf("akbar");
+// console.log(string);
+
+
+//METHOD 08 : startwith() --> the use of this function is to check the false charater of the given string -->boolean output//
+
+// let string = "Qasim";
+// string = string.startsWith("Qas");
+// console.log(string);
+
+
+//METHOD 09 : endwith() function --> the use of this function is to check the last charater of the given string --> boolean output//
+
+// let string = "Qasim akbar";
+// string = string.endsWith("bar");
+// console.log(string);
+
+
+//METHOD 10 : include() function --> if there is an character in the given string so it will print True otherwise false//
+
+// let string = "Qasim";
+// string = string.includes("i");
+// console.log(string);
